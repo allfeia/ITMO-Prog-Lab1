17f84e318a77e3a6c3615006b6ec027a29aeb54b
ALISA <146487164+allfeia@users.noreply.github.com>
2025-01-20 17:05:37 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,25 +1,23 @@
 public class Main {
     public static void main(String[] args) {
         int[] c = new int[9];
-        for (int i = 0; i<c.length; i++){  ////заполнение одномерного массива нечётными числами в порядке убывания от 23 до 7
+        for (int i = 0; i<c.length; i++){  
             c[i] = 23-2*i;
         }
-        double[] x = new double[12];      //создание нового одномерного массива типа double
+        double[] x = new double[12];     
         double min = -14.0;
         double max = 2.0;
-        for(int i = 0; i<x.length; i++){     //int i от 0; пока i<длинны массива x; при каждом вхождении в цикл добавляется +1 к индксу, используемого элемента
-            x[i] = ((double)(Math.random() * Math.abs(min) + (Math.abs(max) - Math.abs(min))));  //разница между модулем макс и мин значения определяет диапазон случайных чисел,
-        //а умножение метода Math.random() на модуль мин значения позволяет получить случайное число в диапазоне от 0 до мин значения.
-        //Добавление разницы между макс и мин значениями позволяет получить случайное число от 0 до разницы между макс и мин значениями
+        for(int i = 0; i<x.length; i++){     
+            x[i] = ((double)(Math.random() * Math.abs(min) + (Math.abs(max) - Math.abs(min))));  
         }
-        double[][] p = new double[9][12]; //создание двумерного массива
-        double degree1;   //объявление переменных над циклом(тк если объявить их в самом цикле, то при каждом вхождении переменная пересоздаётся, из-за чего занимается больше памяти)
+        double[][] p = new double[9][12]; 
+        double degree1;   
         double degree2;
         double degree21;
         double numerator;
         double denominator;
-        for(int i = 0; i<c.length; i++){   //обозначение переменной i как индекс элемента в массиве c
-            for(int j = 0; j<x.length; j++){   //вложенный цикл, обозначение переменной j как индекс элемента в массиве x
+        for(int i = 0; i<c.length; i++){   
+            for(int j = 0; j<x.length; j++){  
                 if(c[i] == 23){
                     degree1 = Math.pow(x[j],x[j]/(1-x[j]));
                     degree2 = Math.pow(0.25*(degree1-1),2);
@@ -35,13 +33,13 @@ public class Main {
                 }
             }
         }
-        for(double result[] : p){    //цикл, который перебирает каждый элемент массива р и присваивает его значение переменной result
-            for(double i : result){   //цикл, который перебирает каждый элемент массива result и присваивает его значение переменной i
-                String str = String.format("%3f", i);   //форматирование строки с тремя знаками после запятой
-                System.out.printf("%8s", str);    //вывод строки с шириной пробела в 8 символов
-                System.out.print("\t");   //вывод символа табуляции для разделения значений
+        for(double result[] : p){    
+            for(double i : result){   
+                String str = String.format("%3f", i);   
+                System.out.printf("%8s", str);    
+                System.out.print("\t");   
             }
-            System.out.println();   //перевод строки для перехода на новую строку после вывода всех значений внутри цикла
+            System.out.println();   
         }
     }
 }
